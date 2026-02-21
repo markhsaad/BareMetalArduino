4aec7ceef07c150939f48c96e852ad4511d91366
Mark Saad <markhsaad@hotmail.com>
2026-02-21 15:18:53 -0500

    add detailed notes explaining the code; upload doc in case one day the urls in the comments break

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
-TARGET = blink
+MCU     = atmega4809
+TARGET  = blink
 SRC_DIR = src
 
+SRC_FILE = $(SRC_DIR)/$(TARGET).c
+OBJ_FILE = $(SRC_DIR)/$(TARGET).o
+BIN_FILE = $(SRC_DIR)/$(TARGET).bin
+HEX_FILE = $(SRC_DIR)/$(TARGET).hex
+
 default:
-	avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega4809 -c -o $(SRC_DIR)/$(TARGET).o $(SRC_DIR)/$(TARGET).c
-	avr-gcc -mmcu=atmega4809 -o $(SRC_DIR)/$(TARGET).bin $(SRC_DIR)/$(TARGET).o
-	avr-objcopy -O ihex -R .eeprom $(SRC_DIR)/$(TARGET).bin $(SRC_DIR)/$(TARGET).hex
-	avrdude -v -c xplainedmini_updi -p m4809 -P usb -U flash:w:$(SRC_DIR)/$(TARGET).hex:i
+	avr-gcc -Os -DF_CPU=16000000UL -mmcu=$(MCU) -c -o $(OBJ_FILE) $(SRC_FILE)
+	avr-gcc -mmcu=$(MCU) -o $(BIN_FILE) $(OBJ_FILE)
+	avr-objcopy -O ihex -R .eeprom $(BIN_FILE) $(HEX_FILE)
+	avrdude -v -c xplainedmini_updi -p m4809 -P usb -U flash:w:$(HEX_FILE):i
+	
+	rm $(OBJ_FILE) $(BIN_FILE) $(HEX_FILE)
--- src/blink.c
+++ src/blink.c
@@ -1,28 +1,56 @@
 /*
-* [A] ATmega4809 Datasheet:
+* [A] Arduino Uno WiFi Rev2 Pinout:
+* https://docs.arduino.cc/resources/pinouts/ABX00021-full-pinout.pdf
+*
+* [B] ATmega4809 Datasheet:
 * https://ww1.microchip.com/downloads/en/DeviceDoc/ATmega4808-4809-Data-Sheet-DS40002173A.pdf
 *
-* [B] Arduino Uno WiFi Rev2 Pinout:
-* https://docs.arduino.cc/resources/pinouts/ABX00021-full-pinout.pdf
+* See also the doc folder.
 */
 
-#define PIN 6 // [B] P1
+// From [A] P1, the built-in LED is connected to PD06 (port D, pin 6) on the MCU.
+#define PORT PORTD
+#define PIN 6
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main() {
-    // [A] P33 ?
+    /*
+    * In the compile command, we have the flag -DF_CPU=16000000UL which sets the MCU CPU speed to 16 MHz.
+    * However, reading online, it seems that the ATmega4809 has a default divison ratio of 6. This means 
+    * that the CPU is running at (16/6) MHz. If we do not account for this, then the command _delay_ms(1000) 
+    * will wait for 1 second based on a 16 MHz CPU speed which is really 6 seconds for a (16/6) MHz clock speed.
+    *
+    * EXPECTED_DELAY_TIME_S = 1
+    *
+    * EXPECTED_CPU_SPEED = 16000000
+    * EXPECTED_CYCLES = EXPECTED_DELAY_TIME_S / (1 / EXPECTED_CPU_SPEED)
+    *                 = 1 / (1 / 16000000)
+    *                 = 16000000
+    *
+    * DIVISON_RATIO = 6
+    * ACTUAL_CPU_SPEED = EXPECTED_CPU_SPEED / DIVISON_RATIO
+    *                  = 16000000 / 6
+    *
+    * ACTUAL_DELAY_TIME_S = EXPECTED_CYCLES * (1 / ACTUAL_CPU_SPEED)
+    *                   = 16000000 * (1 / (16000000 / 6))
+    *                   = 6 
+    *
+    * To fix this, we either need to adjust the compile flag or disable the prescaler. Below, we do the latter.
+    */
+
+    // From [B] P33, this briefly unlocks the necessary register to disable the prescaler for modification.
     CPU_CCP = CCP_IOREG_gc;
     
-    // [A] P92
+    // From [B] P92, this disables the prescaler.
     CLKCTRL.MCLKCTRLB = 0;
 
-    // [A] P148
-    PORTD.DIRSET = PORTD.DIRSET | (1 << PIN);
+    // From [B] P148, this will set the specified pin on the specified port to be an output.
+    PORT.DIRSET = PORT.DIRSET | (1 << PIN);
     while(1) {
-        // [A] P154
-        PORTD.OUTTGL = PORTD.OUTTGL | (1 << PIN);
+        // From [B] P154, this will toggle the specified pin on the specified port.
+        PORT.OUTTGL = PORT.OUTTGL | (1 << PIN);
         _delay_ms(1000); 
     }
 }
